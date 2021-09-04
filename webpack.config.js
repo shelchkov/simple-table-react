986ce8806c31f8777f1466e395373ec83b28f4b2
Yaroslav Shelchkov <shelchkovy@yandex.ru>
2021-09-04 10:37:39 +0400

    feat: set up basic react app

---
+++ webpack.config.js
@@ -0,0 +1,31 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+  entry: path.join(__dirname, "src", "index.tsx"),
+  output: { path: path.join(__dirname, "dist"), filename: "index.js" },
+  mode: process.env.NODE_ENV || "development",
+  resolve: {
+    extensions: [".tsx", ".ts", ".js", ".jsx"]
+  },
+  devServer: { contentBase: path.join(__dirname, "src") },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: ["babel-loader"]
+      },
+      {
+        test: /\.(ts|tsx)$/,
+        exclude: /node_modules/,
+        use: ["ts-loader"],
+      },
+    ]
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: path.join(__dirname, "public", "index.html"),
+    })
+  ]
+}
