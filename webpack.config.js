f53480371da8a9a3fd0e3039aabf7370a896085f
Yaroslav Shelchkov <shelchkovy@yandex.ru>
2021-09-05 10:36:15 +0400

    feat: remove babel and use only ts compiler

--- webpack.config.js
+++ webpack.config.js
@@ -12,11 +12,6 @@ module.exports = {
   module: {
     rules: [
       {
-        test: /\.(js|jsx)$/,
-        exclude: /node_modules/,
-        use: ["babel-loader"]
-      },
-      {
         test: /\.(ts|tsx)$/,
         exclude: /node_modules/,
         use: ["ts-loader"],
