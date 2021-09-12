d3489c434d46b3c4eee7c6d06dd38359c8dc5d34
Yaroslav Shelchkov <shelchkovy@yandex.ru>
2021-09-12 11:45:50 +0400

    fix: fix caching and add tests #5

--- src/lib/__tests__/table/utils.test.ts
+++ src/lib/__tests__/table/utils.test.ts
@@ -1,9 +1,38 @@
-import { noop } from "../../table/utils"
+import { noop, removeEmptyColumns, TableData } from "../../table/utils"
 
 describe("utils", () => {
   describe("noop function", () => {
     it("returns undefined", () => {
       expect(noop()).toEqual(undefined)
+    })
+  })
+
+  describe("removeEmptyColumns function", () => {
+    const headers = ["h1", "h2", "h3"]
+
+    it("returns all headers for empty data", () => {
+      const data: TableData[] = []
+      expect(removeEmptyColumns(headers, data)).toBe(headers)
+    })
+
+    it("returns all headers for complete data", () => {
+      const data: TableData[] = [{ h1: "1", h2: "2", h3: "3" }]
+      expect(removeEmptyColumns(headers, data)).toEqual(headers)
+    })
+
+    it("removes header that's not found in data", () => {
+      const data: TableData[] = [{ h1: "1", h2: "2" }]
+      expect(removeEmptyColumns(headers, data)).toEqual(headers.slice(0, 2))
+    })
+
+    it("checks all entries in data", () => {
+      const data: TableData[] = [
+        { h1: "1", h2: "2" },
+        { h1: "3", h2: "4" },
+      ]
+      expect(removeEmptyColumns(headers, data)).toEqual(headers.slice(0, 2))
+      data[1].h3 = "5"
+      expect(removeEmptyColumns(headers, data)).toEqual(headers)
     })
   })
 })
