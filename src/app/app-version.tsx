4c828af64dbcb7ed20eb670e399ed29228463898
Yaroslav Shelchkov <shelchkovy@yandex.ru>
2023-03-14 07:46:07 +0400

    chore: update app building #25

--- src/app/app-version.tsx
+++ src/app/app-version.tsx
@@ -1,15 +1,16 @@
 import React, { ReactElement } from "react"
 
 export const AppVersion = (): ReactElement => {
-  const version = process.env.CIRCLE_TAG
+  const version = process.env.TAG_NAME
+  let text = "Documentation"
 
-  if (!version) {
-    return <></>
+  if (version) {
+    text = `Latest version: ${version}`
   }
 
   return (
     <div className="app-version">
-      <a href="https://www.npmjs.com/package/simple-table-react">Latest version: {version}</a>
+      <a href="https://www.npmjs.com/package/simple-table-react">{text}</a>
     </div>
   )
 }
